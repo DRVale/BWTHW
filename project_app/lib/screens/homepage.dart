11f822767b26e221c27d6ae63dca5b89f7a333e1
LoreCorti01 <lorenzo.cortinovis@studenti.unipd.it>
2025-05-05 16:36:03 +0200

    Codice LoginPage e HomePage

--- project_app/lib/screens/homepage.dart
+++ project_app/lib/screens/homepage.dart
@@ -1,2 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:project_app/screens/loginpage.dart';
+//import 'package:project_app/screens/counterpage.dart';
+import 'package:project_app/screens/loginPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  static const routename = 'Homepage';
+
+  @override
+  Widget build(BuildContext context) {
+
+    print('${HomePage.routename} built');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(HomePage.routename),
+      ),
+      
+      body: Center(
+          child: Text('login_flow'),
+        ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('login_flow'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () => _toLoginPage(context),
+            ),
+            ListTile(
+              leading: Icon(Icons.plus_one),
+              title: Text('Counterpage'),
+              onTap: () => _toCounterPage(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  } //build
+
+  void _toLoginPage(BuildContext context)async{
+
+    final logoutReset = await SharedPreferences.getInstance();
+    await logoutReset.remove('username');
+    await logoutReset.remove('password');
+
+    //Pop the drawer first 
+    Navigator.pop(context);
+    //Then pop the HomePage
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+  }//_toCalendarPage
+
+  void _toCounterPage(BuildContext context){
+    //Pop the drawer first 
+    Navigator.pop(context);
+    //Then pop the HomePage
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Counterpage(title: 'maracaibo')));
+  }//_toCalendarPage
+
+
+} //HomePage
