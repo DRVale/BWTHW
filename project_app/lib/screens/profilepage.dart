f997a4ad0426ba851914f1779cbe8dfbfbe1c2ae
Valentino De Rivo <valentino.derivo@gmail.com>
2025-05-09 15:28:35 +0200

    Update profilepage.dart

--- project_app/lib/screens/profilepage.dart
+++ project_app/lib/screens/profilepage.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:project_app/screens/homepage.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  static const routename = 'Homepage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
