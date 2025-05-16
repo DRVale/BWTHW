91cb8a3a28efa00746754d5807ce06e03b24929a
virgius <virginia.barbaglia@studenti.unipd.it>
2025-05-16 11:29:33 +0200

    Update profilepage.dart

--- project_app/lib/screens/profilepage.dart
+++ project_app/lib/screens/profilepage.dart
@@ -39,19 +39,23 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:  const Color.fromARGB(255, 250, 250, 238),
-      appBar: AppBar(title: const Text('Modifica nome'),
+      appBar: AppBar(title: const Text('Change name',style: TextStyle(color: Colors.grey),),
       backgroundColor:  const Color.fromARGB(255, 250, 250, 238),),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const Text('Inserisci il tuo nuovo nome:'),
+            const Text('Insert your new name:',style: TextStyle(color: Colors.grey),),
             const SizedBox(height: 10),
             TextField(
+              cursorColor: Colors.black,
               controller: _controller,
               decoration: const InputDecoration(
                 labelText: 'Nuovo nome',
                 labelStyle: TextStyle(color: Colors.green),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green)
+                ),
                 focusedBorder: UnderlineInputBorder(
                   borderSide: BorderSide(color: Colors.green)
                 )
