5e84ad725176ee7a4663393cd9a78b5064fad7aa
Valentino De Rivo <valentino.derivo@gmail.com>
2025-05-21 16:57:10 +0200

    Aggiunto conteggio della distanza totale

--- project_app/lib/screens/optionspage.dart
+++ project_app/lib/screens/optionspage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:project_app/providers/dataprovider.dart';
 import 'package:project_app/models/requesteddata.dart';
+import 'package:project_app/widgets/line_plot.dart';
 
 class OptionsPage extends StatelessWidget {
   const OptionsPage({Key? key}) : super(key: key);
@@ -20,17 +21,39 @@ class OptionsPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
+            Consumer<DataProvider>(builder: (context, data, child) {
+              if (data.distances.length == 0) {
+                return Text('Nothing to display, press the button to fetch the data');
+              }//if
+              else {
+                return DistanceDataPlot(distanceData: data.distances);
+              }//else
+            }),
             ElevatedButton(
               onPressed: () {
                 Provider.of<DataProvider>(context, listen: false).fetchDistanceData('2023-05-13');
-            
-                // Define consumer
 
                 // for debug
                 print('Done');
               },
+              
               child: Text('Prendi i dati'),
             ),
+
+            Consumer<DataProvider>(builder: (context, data, child) {
+              if (data.distances.length == 0) {
+                return Text('');
+              }else{
+                int dataLength = data.distances.length;
+                int totalDistance = 0;
+                for(var i = 0; i < dataLength; i++){
+                  totalDistance = totalDistance + data.distances[i].value;
+                }
+                
+                return Text('The total distance is $totalDistance');
+              }//else
+            }),
+
           ],
         ),
       ),
---
+++ project_app/lib/widgets/line_plot.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:project_app/models/requesteddata.dart';
+
+/// Local import
+
+///Renders default line series chart
+class DistanceDataPlot extends StatelessWidget {
+  ///Creates default line series chart
+  DistanceDataPlot({Key? key, required this.distanceData}) : super(key: key);
+
+  final List<Distance> distanceData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      plotAreaBorderWidth: 0,
+      title: ChartTitle(text: 'Yesterday Distance'),
+      primaryXAxis: const DateTimeAxis(majorGridLines: MajorGridLines(width: 0)),
+      primaryYAxis: const NumericAxis(
+          labelFormat: '{value} meters',
+          axisLine: AxisLine(width: 0),
+          majorTickLines: MajorTickLines(color: Colors.transparent)),
+      series: _getDistanceDataSeries(),
+      tooltipBehavior: TooltipBehavior(enable: true),
+    );
+  }
+
+  /// The method returns line series to chart.
+  List<LineSeries<Distance, DateTime>> _getDistanceDataSeries() {
+    return <LineSeries<Distance, DateTime>>[
+      LineSeries<Distance, DateTime>(
+          dataSource: distanceData,
+          xValueMapper: (data, _) => data.time,
+          yValueMapper: (data, _) => data.value,
+          name: 'Distance',
+          markerSettings: const MarkerSettings(isVisible: true))
+    ];
+  }//_getDistanceDataSeries
+
+}//DistanceDataPlot
