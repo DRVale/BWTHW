4260240a6a65719e5bce82060d272bd6db69516a
LoreCorti01 <lorenzo.cortinovis@studenti.unipd.it>
2025-05-26 16:15:38 +0200

    Update canteenpage.dart
    Listview.builder

--- project_app/lib/screens/canteenpage.dart
+++ project_app/lib/screens/canteenpage.dart
@@ -4,14 +4,16 @@ import 'package:project_app/screens/boxpage.dart';
 class CanteenPage extends StatelessWidget {
    CanteenPage({super.key});
 
-List<Canteen> canteen_list = [
+  List<Canteen> canteen_list = [
     Canteen(canteenAndress: 'Via Roma 6', canteenName: 'Piovegio'),
     Canteen(canteenAndress: 'Via Gradenigo', canteenName: 'Murialdo'),
     Canteen(canteenAndress: 'Via Ugo Bassi', canteenName: 'Pio X'),
     Canteen(canteenAndress: 'Piazza Garibaldi', canteenName: 'Belzoni'),
   ];
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold( 
       backgroundColor: const Color.fromARGB(255, 250, 250, 238),
       appBar: AppBar(
@@ -25,36 +27,28 @@ List<Canteen> canteen_list = [
           SizedBox(height: 40,),
           Expanded(
             child: 
-                    ListView.builder(
-                  
-                  itemCount: canteen_list.length,
-                  // itemBuilder: (context, index) => box_list[index],
-                  itemBuilder: (context, index){
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 10,),
-                        Container( //Aggiunto container per abbellire i pacchi
-                          width: MediaQuery.sizeOf(context).width - 50,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black54),
-                            borderRadius: BorderRadius.all(Radius.circular(20)),                    
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              canteen_list[index],
+              ListView.builder( 
+                itemCount: canteen_list.length,
+                itemBuilder: (context, index){
+                  final canteen = canteen_list[index];  //indice della lista
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
 
-                              SizedBox(height: 10),
-                            ],
+                      ListTile(
+                        leading: const Icon(Icons.add_home, color: Colors.green, size: 20),
+                        title: Text('Canteen: ${canteen.canteenName}',
+                            style: const TextStyle(color: Colors.black54)),
+                        subtitle: Text('Address: ${canteen.canteenAndress}',
+                            style: const TextStyle(fontSize: 9)),
+                        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.green),
+                        onTap: () => _toBoxPage(context, mensa: canteen.canteenName),
                       ),
-                        ),
-                      //SizedBox(height: 60)
-                      ],
-                );
-                  
-                  },
-          ),
+                      const SizedBox(height: 20),
+                    ],
+                  );
+                },
+              ),
             
             
             /*
@@ -131,23 +125,12 @@ List<Canteen> canteen_list = [
   void _toBoxPage(BuildContext context, {required String mensa}){
     Navigator.of(context).push(MaterialPageRoute(builder: (context) => BoxPage(mensa: mensa)));
   }
-
 }
 
 class Canteen{
-  //final String addess;
   final String canteenName;
   final String canteenAndress;
-
-  const Canteen({
-    
-    required this.canteenName,
-    required this.canteenAndress,
-    
-  });
-
-
-
+  const Canteen({required this.canteenName,required this.canteenAndress,});
 }
 
 
