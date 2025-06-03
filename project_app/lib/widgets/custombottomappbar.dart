823d7e4f28000c22b64122a8dbc7265e579e25f7
LoreCorti01 <lorenzo.cortinovis@studenti.unipd.it>
2025-06-03 14:29:26 +0200

    Commit_valentino

--- project_app/lib/widgets/custombottomappbar.dart
+++ project_app/lib/widgets/custombottomappbar.dart
@@ -2,15 +2,8 @@ import 'package:flutter/material.dart';
 
 class CustomBottomAppBar extends StatelessWidget {
 
-  // final VoidCallback toPage1;
-  // final VoidCallback toPage2;
-
-  // list of elements to show
-  final List<String> tabnames;
-  // index currently selected
-  final int currentIndex;
-  // action to perform on tap -> change index
-  final Function(int) callback;
+  final VoidCallback toPage1;
+  final VoidCallback toPage2;
 
   // SE VOGLIAMO RIUTILIZZARE LA CustomNavigationBar, allora tocca mettere in input anche le icone e i nomi
   // final Widget icon1;
@@ -18,12 +11,8 @@ class CustomBottomAppBar extends StatelessWidget {
 
   const CustomBottomAppBar({
     super.key, 
-    // required this.toPage1, 
-    // required this.toPage2,
-
-    required this.tabnames,
-    required this.currentIndex,
-    required this.callback,
+    required this.toPage1, 
+    required this.toPage2,
   });
 
   @override
@@ -48,114 +37,32 @@ class CustomBottomAppBar extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:
-                  tabnames
-                      .asMap() // i need the index of the element so i transform the list to a dict/map
-                      .map(
-                        (k, e) => MapEntry(
-                          k,
-                          TextButton(
-                            onPressed: () => callback(k),
-                            onLongPress: () {
-                              bool isLeft = currentIndex == k? true : false;
-                              showPopup(context, 'Do something', isLeft);
-                            },
-                            child: Text(
-                              e,
-                              style:
-                              TextStyle(
-                                color: currentIndex == k? Colors.black : Colors.grey,
-                                fontWeight: currentIndex == k? FontWeight.bold : null
-                              )
-                            ),
-                          ),
-                        ),
-                      )
-                      .values
-                      .toList(),
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // [
+          children: [
             // Left side of the bar
-            // Expanded(
-            //   child: InkWell(
-            //     onTap: toPage1,
-            //     child: Align(
-            //       alignment: Alignment(-0.5, 0),
-            //       child: Column(
-            //         children: [
-            //           Icon(
-            //               Icons.history, 
-            //               color: Colors.black54,
-            //               size: 30,
-            //             ),
-            //           Text('Homepage')
-            //         ],
-            //       )
-            //     ),
-            //   ),
-            // ),
+            Expanded(child:
+              InkWell(
+              onTap: toPage1,
+              child: Align(
+                alignment: Alignment(-0.5, 0),
+                child: Icon(Icons.auto_graph,color: Colors.black54 ,size: 30,)
+                ),
+              ),
+            ),
 
-            // // Space between the buttons
-            // //SizedBox(width: 150),
+            // Space between the buttons
+            //SizedBox(width: 150),
         
-            // // Right side of the bar
-            // Expanded(
-            //   child: InkWell(
-            //     onTap: toPage2,
-            //     child: Center(
-            //       child: Icon(
-            //         Icons.history,
-            //         color: Colors.black54,
-            //         size: 30,
-            //       )
-            //     ),
-            //   ),
-            // ),
-          // ],
+            // Right side of the bar
+            Expanded(child: 
+              InkWell(
+              onTap: toPage2,
+              child: Center(child: Icon(Icons.history,color: Colors.black54 ,size: 30,)),
+              ),
+            ),
+          ],
         ),
       ),   
     );
-  }
-
-  void showPopup(BuildContext context, String message, bool isLeft) {
-    final overlay = Overlay.of(context);
-    final overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        bottom: 80, // Adjust as needed
-        left: isLeft? 20 : MediaQuery.of(context).size.width - 140,
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              color: Colors.black87,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Text(
-              message,
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ),
-      ),
-    );
-
-    overlay.insert(overlayEntry);
-    Future.delayed(Duration(seconds: 2), () {
-      overlayEntry.remove();
-    });
   }
 }
 
