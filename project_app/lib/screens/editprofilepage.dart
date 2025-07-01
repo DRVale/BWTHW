034d74856d150bef72948565d14dac1e7d98fa47
virgius <virginia.barbaglia@studenti.unipd.it>
2025-07-01 16:36:26 +0200

    Update editprofilepage.dart

--- project_app/lib/screens/editprofilepage.dart
+++ project_app/lib/screens/editprofilepage.dart
@@ -9,25 +9,56 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _surnameController = TextEditingController();
+  final TextEditingController _birthdateController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _loadName();
+    _loadData();
   }
 
-  Future<void> _loadName() async {
+  Future<void> _loadData() async {
     final sp = await SharedPreferences.getInstance();
-    _controller.text = sp.getString('username') ?? '';
+    _nameController.text = sp.getString('username') ?? '';
+    _surnameController.text = sp.getString('surname') ?? '';
+    _birthdateController.text = sp.getString('birthdate') ?? '';
   }
 
-  Future<void> _saveName() async {
-    final name = _controller.text.trim();
-    if (name.isNotEmpty) {
+  Future<void> _saveData() async {
+    final name = _nameController.text.trim();
+    final surname = _surnameController.text.trim();
+    final birthdate = _birthdateController.text.trim();
+    if (name.isNotEmpty && surname.isNotEmpty && birthdate.isNotEmpty) {
       final sp = await SharedPreferences.getInstance();
       await sp.setString('username', name);
+      await sp.setString('surname', surname);
+      await sp.setString('birthdate', birthdate);
       Navigator.pop(context, true); // torna con esito positivo
+    }
+  }
+
+  Future<void> _pickDate() async{
+    DateTime? date = await showDatePicker(context: context, firstDate: DateTime(1900), lastDate: DateTime.now(), initialDate: DateTime(2000),
+    builder: (BuildContext context, Widget? child) {
+      return Theme(
+        data: ThemeData.light().copyWith(
+          primaryColor: Colors.green,
+          colorScheme: ColorScheme.light(
+            primary: Colors.green,
+            onPrimary: Colors.white,
+            surface: Color.fromARGB(255, 250, 250, 238),
+            onSurface: Colors.black54,
+          ),
+          dialogBackgroundColor: Color.fromARGB(255, 250, 250, 238),
+        ),
+        child: child!
+      );
+    }
+  );
+  if (date != null) {
+      _birthdateController.text = "${date.day}/${date.month}/${date.year}";
     }
   }
 
@@ -35,7 +66,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 250, 250, 238),
-      appBar: AppBar(title: Text('Edit Page',style: TextStyle(color: Colors.black54,fontWeight: FontWeight.bold),),
+      appBar: AppBar(title: Text('Change your profile',style: TextStyle(color: Colors.black54,fontWeight: FontWeight.bold),),
       backgroundColor:  const Color.fromARGB(255, 250, 250, 238),),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -43,7 +74,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
           children: [
             SizedBox(height: 10,),
             TextField(
-              controller: _controller,
+              controller: _nameController,
               cursorColor: Colors.black54,
               decoration: InputDecoration(
                 labelText: 'Insert the new name',
@@ -56,12 +87,45 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 )
                 ),
             ),
+            SizedBox(height: 10,),
+            TextField(
+              controller: _surnameController,
+              cursorColor: Colors.black54,
+              decoration: InputDecoration(
+                labelText: 'Insert the new surname',
+                labelStyle: TextStyle(color: Colors.black54),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black54, ), 
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide( color: Colors.green),
+                )
+                ),
+            ),
+            
+            SizedBox(height: 10,),
+            TextField(
+              controller: _birthdateController,
+              readOnly: true,
+              onTap: _pickDate,
+              decoration: InputDecoration(
+
+                labelText: 'Date of bith',
+                labelStyle: TextStyle(color: Colors.black54),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black54),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+              ),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 107, 165, 109),
                   foregroundColor: Colors.black54,),
-              onPressed: _saveName,
+              onPressed: _saveData,
               child: Text('Save'),
             ),
           ],
